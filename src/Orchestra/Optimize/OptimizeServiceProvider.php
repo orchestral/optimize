c8eb5fe019759e587289e931ccf10ceb9476479e
Mior Muhammad Zaki <crynobone@gmail.com>
2013-10-31 01:41:52 +0800

    Update service provider to use bindShared.
    Signed-off-by: Mior Muhammad Zaki <crynobone@gmail.com>

--- src/Orchestra/Optimize/OptimizeServiceProvider.php
+++ src/Orchestra/Optimize/OptimizeServiceProvider.php
@@ -18,7 +18,7 @@ class OptimizeServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        $this->app['orchestra.commands.optimize'] = $this->app->share(function () {
+        $this->app->bindShared('orchestra.commands.optimize', function () {
             $components = require __DIR__."/compile.php";
             return new OptimizeCommand($components);
         });
